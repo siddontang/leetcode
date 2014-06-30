7fa7287937e16e074bf18817e20540c7c01c518a
siddontang <siddontang@gmail.com>
2014-06-30 23:24:05 +0800

    best time to buy and sell stock ii

---
+++ best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,24 @@
+#include "leetcode.h"
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int len = (int)prices.size();
+        if(len <= 1) {
+            return 0;
+        }
+
+        int sum = 0;
+        for(int i = 1; i < len; i++) {
+            if(prices[i] - prices[i - 1] > 0) {
+                sum += prices[i] - prices[i - 1];
+            }
+        }
+
+        return sum;
+    }
+};
+
+int main() {
+    return 0;
+}
