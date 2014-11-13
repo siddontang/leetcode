baefcac261c35e167d1d33fd02455c07f54a3a0e
siddontang <siddontang@gmail.com>
2014-11-13 22:41:34 +0800

    update pascals triangle ii

--- pascals-triangle-ii.cpp
+++ pascals-triangle-ii.cpp
@@ -4,25 +4,17 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int> vals;
-        vector<int> last;
+        
+        vals.resize(rowIndex + 1, 1);
 
-        last.resize(rowIndex + 1);
-        vals.resize(rowIndex + 1);
-
-        last[0] = 1;
-
-        for(int i = 0; i < rowIndex + 1; i++) {
-            vals[0] = 1;
-            vals[i] = 1;
-
-            for(int j = 1; j < i; j++) {
-                vals[j] = last[j -1] + last[j];
+        for(int i = 0; i < rowIndex + 1; ++i) {
+            for(int j = i - 1; j >= 1; --j) {
+                vals[j] = vals[j] + vals[j - 1];
             }
 
-            vals.swap(last);
         }
 
-        return last;
+        return vals;
     }
 };
 
